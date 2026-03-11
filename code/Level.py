b3d70345c1ef9f5581c852f7328c19daeb2e98ff
GabrielCruz <gabri712289@gmail.com>
2026-03-11 16:12:04 -0400

    Adicionei efeito parallax no background

--- code/Level.py
+++ code/Level.py
@@ -1,3 +1,6 @@
+from code.EntityFactory import EntityFactory
+
+import pygame.display
 from code.Entity import Entity
 
 
@@ -7,7 +10,12 @@ class Level:
         self.name = name
         self.game_mode = game_mode
         self.entity_list: list[Entity] = []
+        self.entity_list.extend(EntityFactory.get_entity('Level1Bg'))
 
 
     def run(self):
-        pass
+        while True:
+            for ent in self.entity_list:
+                self.window.blit(source=ent.surf, dest = ent.rect)
+                ent.move()
+            pygame.display.flip()
